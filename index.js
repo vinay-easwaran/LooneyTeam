9ee3cfe94c16faff993797569dcd2e05623d05b5
Vinay <vinayeaswaran@yahoo.co.in>
2019-05-30 23:36:42 -0700

    CORS policy update

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express()
 const port = process.env.PORT || 3000
 const db = require('./queries')
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin","*");
+	res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.use(bodyParser.json())
 app.use(
 	bodyParser.urlencoded({

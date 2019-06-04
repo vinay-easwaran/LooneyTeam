32c551cbc817a69dc6cb007ac75cdb8f09f4f198
Vinay <vinayeaswaran@yahoo.co.in>
2019-06-04 12:27:53 -0700

    bug fix attemp #5

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express')
-const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
 const db = require('./queries')
-
-app.use(cors());
 
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin","*");

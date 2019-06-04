eccf88ef1ea2f9f82a33c8f54e81f9b11341f2e1
Vinay <vinayeaswaran@yahoo.co.in>
2019-06-03 19:03:06 -0700

    bug fix attempt #2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const db = require('./queries')
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin","*");
 	res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
-	res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 	next();
 });
 

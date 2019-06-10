adf10886a608d8af97c11e0f8d0e1e7e9487d051
Vinay <vinayeaswaran@yahoo.co.in>
2019-06-10 01:32:25 -0700

    cors npm module update

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
 const db = require('./queries')
-// var cors = require('cors')
-// var clientController = require('../components/clients/controller')
+var cors = require('cors')
+var clientController = require('../components/clients/controller')
 
 
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin","*");
 	res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 
 	next();
-});
+}); */
+
+app.use(cors())
 
 app.use(bodyParser.json())
 app.use(

0f32cffb249646f92ec8984a0ef971e6315bbb57
Vinay <vinayeaswaran@yahoo.co.in>
2019-06-10 01:03:55 -0700

    bug fix attempt #13

--- index.js
+++ index.js
@@ -26,12 +26,7 @@ app.get('/', (request, response) => {
 	response.json({ info: 'LooneyTeam Backend API made with Node.js, Express, and Postgres' })
 })
 
-app.options('/*', function(req, res, next){
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Acess-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-	res.send(200);
-});
+app.options('/*', function() { return; })
 
 app.get('/teachers',db.getTeachers)
 app.get('/teachers/:query',db.getTeacherByParameter)

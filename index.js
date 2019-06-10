5e1bf1f6e3dab8540930ee33beb3dcb1208afe1c
Vinay <vinayeaswaran@yahoo.co.in>
2019-06-10 01:01:23 -0700

    bug fix attempt #12

--- index.js
+++ index.js
@@ -11,14 +11,8 @@ app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin","*");
 	res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-	
-	if ('OPTIONS' == req.method) {
-		res.send(200);
-	}
-	else {
-		next();
-	}
-	// next();
+
+	next();
 });
 
 app.use(bodyParser.json())

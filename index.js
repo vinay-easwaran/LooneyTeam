deaf7677fc5f24ffc62f13d09180b7e70a56b573
Vinay <vinayeaswaran@yahoo.co.in>
2019-08-07 15:41:34 -0700

    capitalized content-type - bug fix attempt

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var cors = require('cors')
 
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin","*");
-	res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, content-type, Accept");
+	res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 
 	if ('OPTIONS' == req.method) {
